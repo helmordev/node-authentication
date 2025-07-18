177d9fe5eae9bb80ce199b09c5b9ec9a213fad43
Poging Programmer <helmorpogi@gmail.com>
2025-07-18 19:21:05 +0800

    fix(schema): add a comment to the optional tables

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,11 +1,9 @@
-import { refreshToken } from "better-auth/api";
 import {
   mysqlTable as table,
   index,
   varchar,
   boolean,
   timestamp,
-  date,
 } from "drizzle-orm/mysql-core";
 
 export const user = table(
@@ -34,7 +32,7 @@ export const session = table(
       .references(() => user.id, { onDelete: "cascade" })
       .notNull(),
     token: varchar("token", { length: 255 }).notNull(),
-    expiresAt: date("expiresAt").notNull(),
+    expiresAt: timestamp("expiresAt").notNull(),
     ipAddress: varchar("ipAddress", { length: 255 }),
     userAgent: varchar("userAgent", { length: 255 }),
     createdAt: timestamp("createdAt").defaultNow().notNull(),
@@ -43,41 +41,45 @@ export const session = table(
   (table) => [index("idx_session_token").on(table.token)],
 );
 
-export const account = table(
-  "account",
-  {
-    id: varchar("id", { length: 255 }).primaryKey(),
-    userId: varchar("userId")
-      .references(() => user.id, { onDelete: "cascade" })
-      .notNull(),
-    accountId: varchar("accountId", { length: 255 }).notNull(),
-    providerId: varchar("providerId", { length: 255 }).notNull(),
-    accessToken: varchar("accessToken", { length: 255 }).notNull(),
-    refreshToken: varchar("refreshToken", { length: 255 }).notNull(),
-    accessTokenExpiresAt: date("accessTokenExpiresAt").notNull(),
-    refreshTokenExpiresAt: date("refreshTokenExpiresAt").notNull(),
-    scope: varchar("scope", { length: 255 }).notNull(),
-    idToken: varchar("scope", { length: 255 }).notNull(),
-    password: varchar("password", { length: 255 }).notNull(),
-    createdAt: timestamp("createdAt").defaultNow().notNull(),
-    updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow().notNull(),
-  },
-  (table) => [
-    index("idx_account_accountId").on(table.accountId),
-    index("idx_account_providerId").on(table.providerId),
-    index("idx_account_accessToken").on(table.accessToken),
-    index("idx_accont_refreshToken").on(table.refreshToken),
-    index("idx_account_scope").on(table.scope),
-    index("idx_account_idToken").on(table.idToken),
-    index("idx_account_password").on(table.password),
-  ],
-);
+/**
+ * The tables below is for OAuth providers
+ **/
 
-export const verification = table("verification", {
-  id: varchar("id", { length: 255 }).primaryKey(),
-  identifier: varchar("identifier", { length: 255 }).notNull(),
-  value: varchar("value", { length: 255 }).notNull(),
-  expiresAt: date("expiresAt").notNull(),
-  createdAt: timestamp("createdAt").defaultNow().notNull(),
-  updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow().notNull(),
-});
+// export const account = table(
+//   "account",
+//   {
+//     id: varchar("id", { length: 255 }).primaryKey(),
+//     userId: varchar("userId")
+//       .references(() => user.id, { onDelete: "cascade" })
+//       .notNull(),
+//     accountId: varchar("accountId", { length: 255 }).notNull(),
+//     providerId: varchar("providerId", { length: 255 }).notNull(),
+//     accessToken: varchar("accessToken", { length: 255 }).notNull(),
+//     refreshToken: varchar("refreshToken", { length: 255 }).notNull(),
+//     accessTokenExpiresAt: timestamp("accessTokenExpiresAt").notNull(),
+//     refreshTokenExpiresAt: timestamp("refreshTokenExpiresAt").notNull(),
+//     scope: varchar("scope", { length: 255 }).notNull(),
+//     idToken: varchar("scope", { length: 255 }).notNull(),
+//     password: varchar("password", { length: 255 }).notNull(),
+//     createdAt: timestamp("createdAt").defaultNow().notNull(),
+//     updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow().notNull(),
+//   },
+//   (table) => [
+//     index("idx_account_accountId").on(table.accountId),
+//     index("idx_account_providerId").on(table.providerId),
+//     index("idx_account_accessToken").on(table.accessToken),
+//     index("idx_accont_refreshToken").on(table.refreshToken),
+//     index("idx_account_scope").on(table.scope),
+//     index("idx_account_idToken").on(table.idToken),
+//     index("idx_account_password").on(table.password),
+//   ],
+// );
+//
+// export const verification = table("verification", {
+//   id: varchar("id", { length: 255 }).primaryKey(),
+//   identifier: varchar("identifier", { length: 255 }).notNull(),
+//   value: varchar("value", { length: 255 }).notNull(),
+//   expiresAt: timestamp("expiresAt").notNull(),
+//   createdAt: timestamp("createdAt").defaultNow().notNull(),
+//   updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow().notNull(),
+// });
